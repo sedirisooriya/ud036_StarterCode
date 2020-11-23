222cc24e988c00d1de1c3fb4d5e5b883fe84aeb4
sedirisooriya <66367354+sedirisooriya@users.noreply.github.com>
2020-11-23 08:08:57 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
+#import webbrowser module
 import webbrowser
 
 class Movie():
+    #init function
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    #function to play movie trailer video
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
